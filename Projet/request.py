8b09ec8e352f428729fe5e0600ee74aa258dcc20
Labop2 <labop2@laboratoire.uqac.ca>
2019-11-13 10:41:21 -0500

    Enregistre plusieurs images sur le bureau

--- Projet/request.py
+++ Projet/request.py
@@ -1,8 +1,27 @@
 import requests
 import shutil
+import time
+import ffmpeg
 
 url = 'http://172.16.12.131/Streaming/channels/1/picture'
-reponse = requests.get(url, auth=('admin','linux111'))
-if reponse.status_code == 200:
-    with open(r"C:\Users\labop2\Desktop\hrllo.jpg", 'wb') as f:
-        f.write(reponse.content)
+# i = 0
+
+
+
+# while i < 10:
+    # reponse = requests.get(url, auth=('admin','linux111'))
+    # if reponse.status_code == 200:
+        # with open(location, 'wb') as f:
+            # f.write(reponse.content)
+    # i++
+    # location.replace('0',i)
+
+for i in range(10):
+	reponse = requests.get(url, auth=('admin', 'linux111'))
+	if reponse.status_code == 200:
+		location = r"C:\Users\labop2\Desktop\images\img" + str(i) + ".jpg"
+		print (location)
+		with open(location, 'wb') as f:
+			f.write(reponse.content)
+
+
