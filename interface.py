7e9ff5514e3d9b7dcc5f172d33dd7e16039dc261
Labop2 <labop2@laboratoire.uqac.ca>
2019-11-27 10:44:27 -0500

    Permet d'avoir un flux vidéo

--- interface.py
+++ interface.py
@@ -1,28 +1,92 @@
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk
 from PIL import Image
-number = 1
-
-def change_img():
-    global number
-    if number == 10:
-        number = 0
-    img = ImageTk.PhotoImage(Image.open(r"C:\Users\labop2\Desktop\images/img"+ str(number) +".jpg"))
-    label.configure(image = img)
-    label.image = img
-    number = number + 1
-    window.after(1000,change_img)
+import requests
+from io import BytesIO
+import os
+from datetime import datetime
+import time
 
 
+class interface():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("1600x900")       
+        self.img = self.get_photo()
+        self.photo_name_list = []
 
-window = Tk()
+        self.button = tk.Button(self.root, text="Fermer", command=self.root.destroy)
+        self.button.pack()
 
+        #TODO: resize the image 
+        self.image = Image.open(BytesIO(self.img))
+        self.image = ImageTk.PhotoImage(self.image)
+        self.label = tk.Label(self.root, image=self.image)
+        self.label.pack()
 
+        self.loop_saving_photos()
+        self.root.after(500, self.change_img)
+        # self.change_img()
+        self.root.mainloop()
 
-img = ImageTk.PhotoImage(Image.open(r"C:\Users\labop2\Desktop\images/img0.jpg"))
-label = Label(window, image= img)
-label.pack()
+    @staticmethod
+    def get_photo():
+        """Get a photo from the IP camera"""       
+        url = 'http://172.16.12.131/Streaming/channels/1/picture'
+        reponse = requests.get(url, auth=('admin','linux111'))
+        if reponse.status_code == 200:
+            return reponse.content
+        else:
+            raise ValueError("Can't get any response")
 
-window.after(1000,change_img)
+    @staticmethod
+    def create_dir_images():
+        """Create a directory named 'images' in the current directory where images will be saved"""
+        path = os.getcwd() + "/images"
+        try:
+            os.makedirs(path)
+        except OSError:
+            print("Creation of the directory "+ path + " failed")
 
-window.mainloop()
+    @staticmethod
+    def get_time():
+        """Get time as a string : dd-mm-yy-hhmmss"""
+        now = datetime.now()
+        str_time = now.strftime("%m-%d-%Y-%H%M%S")
+        return str_time
+
+    def change_img(self):
+        self.image = self.get_photo()
+        self.image = Image.open(BytesIO(self.image))
+        self.image = ImageTk.PhotoImage(self.image)
+        self.label.configure(image=self.image)
+        self.label.image = self.image
+        self.root.after(500, self.change_img)
+
+    def save_photo(self):
+        """Save a photo .gif from the IP camera in the created directory 'images'. Its
+        name is composed of the date and the hour of its capture"""
+        photo = self.get_photo()
+        if os.path.isdir("./images"):
+            time = self.get_time()
+            self.photo_name_list.append(time)
+            location = "./images/" + time + ".jpg"
+            with open(location, "wb") as f:
+                f.write(photo)
+            return self.photo_name_list
+        else:
+            self.create_dir_images()
+            self.save_photo()
+
+    def loop_saving_photos(self):
+        """Infinite loop where we save a new picture in our images directory each 2 seconds"""
+        self.photo_name_list = self.save_photo()
+        time.sleep(2)
+        if len(self.photo_name_list) > 20:
+            for photo in range(0,10):
+                os.remove("./images/" + self.photo_name_list[photo] + ".jpg")
+            del self.photo_name_list[0:10]
+        self.root.after(500, self.loop_saving_photos)
+
+if __name__ == "__main__":
+    interface = interface()
