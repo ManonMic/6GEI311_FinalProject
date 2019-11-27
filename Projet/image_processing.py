73c27786552d24f0b3eaf248b4b5f6604fb43f20
Plourde <jmichel.plourde@gmail.com>
2019-11-27 10:38:44 -0500

    Apply the color on the RGB image
    Il reste à convertir le bitwise grayscale en black and white

--- Projet/image_processing.py
+++ Projet/image_processing.py
@@ -11,8 +11,9 @@ from PIL import Image
 from scipy.linalg import norm
 from scipy.ndimage import gaussian_filter as gaussian
 import requests
-from skimage.filters import gaussian
 from skimage.color import label2rgb
+from skimage.draw import polygon_perimeter, set_color
+from skimage.filters import gaussian
 from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
 from skimage.segmentation import clear_border
@@ -30,11 +31,15 @@ def get_photo(img):
     # response = requests.get(url, auth=('admin', 'linux111'))
     # return response.content
     if img == 1:
-        with open("data/image1.jpg", "rb") as image:
+        with open("data/tester1.jpg", "rb") as image:
             f = image.read()
             b = bytearray(f)
     if img == 2:
-        with open("data/image2.jpg", "rb") as image:
+        with open("data/tester2.jpg", "rb") as image:
+            f = image.read()
+            b = bytearray(f)
+    if img == 3:
+        with open("data/tester3.jpg", "rb") as image:
             f = image.read()
             b = bytearray(f)
     return b
@@ -67,29 +72,24 @@ def pre_treat_img(img):
 if __name__ == "__main__":
     img1 = get_photo(1)
     img2 = get_photo(2)
+    img3 = get_photo(3)
     img1_treated = pre_treat_img(img1)
     img2_treated = pre_treat_img(img2)
-    img1 = resize_img(to_np_array(to_grayscale(img1)))
+    img3_treated = pre_treat_img(img3)
+    output_img = resize_img(to_np_array(Image.open(BytesIO(img3))))
 
-    img_diff = np.abs(img2_treated - img1_treated)
-    img_diff2 = np.abs(img1_treated - img2_treated)
+    img_diff = np.abs(img3_treated - img2_treated)
+    img_diff2 = np.abs(img3_treated - img1_treated)
+    imshow(img_diff2)
     bw_img = np.bitwise_and(img_diff, img_diff2)
-    bw = closing(img_diff > 10, square(3))
+    bw = closing(bw_img > 10, square(3))
     cleared = clear_border(bw)
 
     label_image = label(cleared)
-    img_label_overlay = label2rgb(label_image, image=img1)
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.imshow(img_label_overlay)
-
     for region in regionprops(label_image):
         if region.area >= 100:
             minr, minc, maxr, maxc = region.bbox
-            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                      fill=False, edgecolor='red', linewidth=2)
-            ax.add_patch(rect)
-
-    ax.set_axis_off()
-    plt.tight_layout()
-    plt.show()
+            rr, cc = polygon_perimeter([minr-1, maxr-1, maxr-1, minr-1],
+                                       [minc-1, minc-1, maxc-1, maxc-1])
+            set_color(output_img, (rr, cc), [255, 0, 0])
+    imshow(output_img)
