fe9df2fd5ba529de90d75ab26cddd6e913cbe914
Plourde <jmichel.plourde@gmail.com>
2019-12-04 09:24:58 -0500

    Update code to Python standards

--- send_email.py
+++ send_email.py
@@ -4,7 +4,8 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 import os
 
-def SendMail(dest, subject, body, ImgFileName = None):
+
+def send_email(dest, subject, body, body_img_file_name = None):
 	mail = MIMEMultipart()
 	mail['From'] = 'cameraexpress123@gmail.com'
 	mail['Pass'] = 'LucasEnRetard123'
@@ -13,23 +14,24 @@ def SendMail(dest, subject, body, ImgFileName = None):
 	text = MIMEText(body)
 	mail.attach(text)
 	
-	if ImgFileName != None:		
-		img_data = open(ImgFileName, 'rb').read()
-		image = MIMEImage(img_data, _subtype="jpg", name=os.path.basename(ImgFileName))
+	if body_img_file_name:
+		img_data = open(body_img_file_name, 'rb').read()
+		image = MIMEImage(img_data, _subtype="jpg", name=os.path.basename(body_img_file_name))
 		mail.attach(image)
 	
 	try:
 		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		server.ehlo()
 		server.login(mail['From'], mail['Pass'])
-	except:
+	except ConnectionError:
 		print('Login error')
 		return
+
 	try:
 		server.sendmail(mail['From'], mail['To'], mail.as_string())
 		print('Email sent')
-		return 
 	except:
 		print('Error sending email')
-		return 
+		return
+
 	server.quit()
