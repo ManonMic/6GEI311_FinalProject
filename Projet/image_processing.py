727665a23bd3750e408bfbbc949661e9c2d421b6
Plourde <jmichel.plourde@gmail.com>
2019-12-04 09:17:25 -0500

    Remove class

--- Projet/image_processing.py
+++ Projet/image_processing.py
@@ -10,7 +10,6 @@ import numpy as np
 from PIL import Image
 from scipy.linalg import norm
 from scipy.ndimage import gaussian_filter as gaussian
-import requests
 from skimage.color import label2rgb
 from skimage.draw import polygon_perimeter, set_color
 from skimage.filters import gaussian
@@ -19,14 +18,7 @@ from skimage.morphology import closing, square
 from skimage.segmentation import clear_border
 
 
-def get_photo(img):
-    """Get a photo from the IP camera"""
-    # url = 'http://172.16.12.131/Streaming/channels/1/picture'
-    # response = requests.get(url, auth=('admin', 'linux111'))
-    # if response.status_code != 200:
-    #     raise ConnectionError("Could not connect to the ip camera: {}".format(url))
-    # return response.content
-
+def get_photo_offline(img):
     if img == 1:
         with open("data/tester1.jpg", "rb") as image:
             f = image.read()
@@ -48,96 +40,93 @@ def imshow(arr):
     plt.show()
 
 
-class ImageProcessor:
-    """Analyze a series of images and moving elements in it"""
+@staticmethod
+def _to_np_array(byte_stream):
+    arr = np.array(byte_stream)
+    return arr.astype(np.int16)
 
-    @staticmethod
-    def _to_np_array(byte_stream):
-        arr = np.array(byte_stream)
-        return arr.astype(np.int16)
+@staticmethod
+def _resize_img(arr):
+    """This ratio was chosen to get rid of the camera UI text"""
+    return arr[130:1350, :]
 
-    @staticmethod
-    def _resize_img(arr):
-        """This ratio was chosen to get rid of the camera UI text"""
-        return arr[130:1350, :]
+@staticmethod
+def _open_as_bytestream(img):
+    return Image.open(BytesIO(img))
 
-    @staticmethod
-    def _open_as_bytestream(img):
-        return Image.open(BytesIO(img))
+def _to_grayscale(arr):
+    img_bytes = _open_as_bytestream(arr)
+    img_bytes = img_bytes.convert('L')
+    return img_bytes
 
-    def _to_grayscale(self, arr):
-        img_bytes = self._open_as_bytestream(arr)
-        img_bytes = img_bytes.convert('L')
-        return img_bytes
+def _prepare_image(img):
+    """Convert an image byte stream to grayscale into an array and applies a Gaussian blur to reduce noise """
+    arr = _to_grayscale(img)
+    arr = _to_np_array(arr)
+    arr = _resize_img(arr)
+    arr = gaussian(arr, sigma=3, preserve_range=True)
+    arr = _to_np_array(arr)
+    return arr
 
-    def _prepare_image(self, img):
-        """Convert an image byte stream to grayscale into an array and applies a Gaussian blur to reduce noise """
-        arr = self._to_grayscale(img)
-        arr = self._to_np_array(arr)
-        arr = self._resize_img(arr)
-        arr = gaussian(arr, sigma=3, preserve_range=True)
-        arr = self._to_np_array(arr)
-        return arr
+@staticmethod
+def _subtract_images(img1, img2):
+    """Pixel per pixel differentiation and turning the result to black and white"""
+    diff = np.abs(img2 - img1)
+    diff = np.where(diff > 15, 255, 0)
+    return diff
 
-    @staticmethod
-    def _subtract_images(img1, img2):
-        """Pixel per pixel differentiation and turning the result to black and white"""
-        diff = np.abs(img2 - img1)
-        diff = np.where(diff > 15, 255, 0)
-        return diff
+def process(img_arr):
+    if len(img_arr) != 3:
+        raise ValueError(
+            "process requires an array of 3 images: img_arr parameter contains {} elements".format(len(img_arr))
+        )
 
-    def process(self, img_arr):
-        if len(img_arr) != 3:
-            raise ValueError(
-                "process requires an array of 3 images: img_arr parameter contains {} elements".format(len(img_arr))
-            )
+    # step 2 : Prepare images
+    img1_prepared = _prepare_image(img_arr[0])
+    img2_prepared = _prepare_image(img_arr[1])
+    img3_prepared = _prepare_image(img_arr[2])
 
-        # step 2 : Prepare images
-        img1_prepared = self._prepare_image(img_arr[0])
-        img2_prepared = self._prepare_image(img_arr[1])
-        img3_prepared = self._prepare_image(img_arr[2])
+    # step 3 : resize a copy of the last image to use as the output
+    output_img = _open_as_bytestream(img_arr[-1])
+    output_img = _to_np_array(output_img)
+    output_img = _resize_img(output_img)
 
-        # step 3 : resize a copy of the last image to use as the output
-        output_img = self._open_as_bytestream(img_arr[-1])
-        output_img = self._to_np_array(output_img)
-        output_img = self._resize_img(output_img)
+    # step 4 : make a diff of each image vs its predecessor
+    img_diff = _subtract_images(img3_prepared, img2_prepared)
+    img_diff2 = _subtract_images(img3_prepared, img1_prepared)
 
-        # step 4 : make a diff of each image vs its predecessor
-        img_diff = self._subtract_images(img3_prepared, img2_prepared)
-        img_diff2 = self._subtract_images(img3_prepared, img1_prepared)
+    # step 5: Do a bitwise comparison between the two differentiations
+    bw_img = np.bitwise_and(img_diff2, img_diff)
 
-        # step 5: Do a bitwise comparison between the two differentiations
-        bw_img = np.bitwise_and(img_diff2, img_diff)
+    # step 6 : apply a threshold to remove gaps
+    bw = closing(bw_img > 0, square(3))
+    cleared = clear_border(bw)
 
-        # step 6 : apply a threshold to remove gaps
-        bw = closing(bw_img > 0, square(3))
-        cleared = clear_border(bw)
+    # step 7 : label image regions
+    label_image = label(cleared)
 
-        # step 7 : label image regions
-        label_image = label(cleared)
+    # step 8 : draw red box mask around large enough regions and apply to output image
+    movement = False
+    for region in regionprops(label_image):
+        if region.area >= 200:
+            movement = True
+            minr, minc, maxr, maxc = region.bbox
+            rr, cc = polygon_perimeter([minr-1, maxr-1, maxr-1, minr-1],
+                                       [minc-1, minc-1, maxc-1, maxc-1])
+            set_color(output_img, (rr, cc), [255, 0, 0])
 
-        # step 8 : draw red box mask around large enough regions and apply to output image
-        movement = False
-        for region in regionprops(label_image):
-            if region.area >= 200:
-                movement = True
-                minr, minc, maxr, maxc = region.bbox
-                rr, cc = polygon_perimeter([minr-1, maxr-1, maxr-1, minr-1],
-                                           [minc-1, minc-1, maxc-1, maxc-1])
-                set_color(output_img, (rr, cc), [255, 0, 0])
-
-        return output_img, movement
+    return output_img, movement
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # step 1 : acquire images
-    img1 = get_photo(1)
-    img2 = get_photo(2)
-    img3 = get_photo(3)
-
-    img_pr = ImageProcessor()
-    result_img, someone_broke_in = img_pr.process([img1, img2, img3])
-
-    imshow(result_img)
-    if someone_broke_in:
-        print("oh shoot someone broke in!")
+    # img1 = get_photo_offline(1)
+    # img2 = get_photo_offline(2)
+    # img3 = get_photo_offline(3)
+    #
+    # img_pr = ImageProcessor()
+    # result_img, someone_broke_in = img_pr.process([img1, img2, img3])
+    #
+    # imshow(result_img)
+    # if someone_broke_in:
+    #     print("oh shoot someone broke in!")
