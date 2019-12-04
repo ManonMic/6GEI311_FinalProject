ea136a2af705b49f33bd48642a4a414ed30aa020
Plourde <jmichel.plourde@gmail.com>
2019-12-04 11:04:18 -0500

    Add img acquisition on thread

--- Projet/live_cam_sec.py
+++ Projet/live_cam_sec.py
@@ -1,4 +1,39 @@
-from image_processing import process
+from threading import Thread, Event
+
+from request import get_photo
+from image_processing import process, imshow
+import interface
+
+
+img_list = []
+
+
+def get_img():
+    img_list.append(get_photo())
+    print("an image was acquired")
+
+
+class GetImgThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(1):
+            get_img()
 
 
 if __name__ == "__main__":
+    # interface = interface()
+    stop_flag = Event()
+    img_thread = GetImgThread(stop_flag)
+    img_thread.start()
+    while True:
+        if len(img_list) > 3:
+            imgs = []
+            for i in range(3):
+                imgs.append(img_list[i])
+            del img_list[0]
+            output_img, movement = process(imgs)
+            imshow(output_img)
+            # change image
